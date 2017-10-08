ba6010ceeb916de8280377c254331234b718fe71
Raul Riera <rieraraul@gmail.com>
2017-10-08 13:24:13 -0400

    Updated to Swift 4, removed the "wrapping" navigation controller

--- AuthenticationViewController/AuthenticationViewController/AuthenticantionViewController.swift
+++ AuthenticationViewController/AuthenticationViewController/AuthenticantionViewController.swift
@@ -25,103 +25,67 @@ public enum AuthenticationError: Error {
 }
 
 /**
- The AuthenticationViewController class provides a standard interface for authenticating to oauth 2.0 protected endpoints via SFSafariViewController.
- 
- Use the `AuthenticationHandler` and or `FailureHandler` to dismiss the View Controller once you are finished with the authentication.
- */
-open class AuthenticationViewController: UINavigationController {
-    public typealias AuthenticationHandler = ([String: AnyObject]) -> Void
-    public typealias FailureHandler = (AuthenticationError) -> Void
-    /// A closure called when the authentication token is retrieved
-    open var authenticationHandler: AuthenticationHandler?
-    /// A closure called when an error occurs in any of the authentication step
-    open var failureHandler: FailureHandler?
-    
-    fileprivate let provider: AuthenticationProvider
-    
-    // MARK: Initialisers
-    
-    /**
-    Returns a newly initialized authentication view controller with the specified provider.
-    
-    - parameter provider:	The provider to authenticate against to
-    
-    - returns: A newly initialized AuthenticationViewController object.
-    */
-    public init(provider: AuthenticationProvider) {
-        self.provider = provider
-        
-        super.init(nibName: nil, bundle: nil)
-    }
+The AuthenticationViewController class provides a standard interface for authenticating to oauth 2.0 protected endpoints via SFSafariViewController.
 
-    required public init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) is not supported. Did you mean to use init(provider:)?")
-    }
-    
-    // MARK: View Controller Lifecycle
-    
-    override open func viewDidLoad() {
-        super.viewDidLoad()
-        
-        let authenticationURL = provider.authorizationURL
-        let safariViewController = SFSafariViewController(url: authenticationURL as URL)
-        safariViewController.title = provider.title
-        
-        let cancelButton = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(AuthenticationViewController.didTapCancel))
-        safariViewController.navigationItem.rightBarButtonItem = cancelButton
-        setViewControllers([safariViewController], animated: false)
-    }
-    
-    // MARK: Actions
-    
-    internal func didTapCancel() {
-        failureHandler?(.userCancelled)
-    }
-    
-    // MARK: Authentication
-    
-    /**
-    Initiates the process of exchanging the authentication code for an actual authentication token. If everything goes well the `AuthenticationHandler` will be executed, otherwise listen for the `FailureHandler`.
-    
-    - parameter code:	The authorization code retrieved
-    */
-    open func authenticateWithCode(_ code: String) {
-        let request = NSMutableURLRequest(url: provider.accessTokenURL as URL)
-        let parameters = provider.parameters.map { key, value in
-            "\(key)=\(value)"
-            }.joined(separator: "&")
-        let data = "client_id=\(provider.clientId)&client_secret=\(provider.clientSecret)&code=\(code)&\(parameters)"
-        
-        request.httpMethod = "POST"
-        request.httpBody = data.data(using: String.Encoding.utf8)
-
-        URLSession.shared.dataTask(with: request as URLRequest) { [unowned self] (data, response, error) in
+Use the `AuthenticationHandler` and or `FailureHandler` to dismiss the View Controller once you are finished with the authentication.
+*/
+open class AuthenticationViewController: SFSafariViewController {
+	public typealias AuthenticationHandler = ([String: AnyObject]) -> Void
+	public typealias FailureHandler = (AuthenticationError) -> Void
+	/// A closure called when the authentication token is retrieved
+	open var authenticationHandler: AuthenticationHandler?
+	/// A closure called when an error occurs in any of the authentication step
+	open var failureHandler: FailureHandler?
+	let provider: AuthenticationProvider
+	
+	required public init(provider: AuthenticationProvider) {
+		self.provider = provider
+		
+		super.init(url: provider.authorizationURL, entersReaderIfAvailable: false)
+	}
+	
+	/**
+	Initiates the process of exchanging the authentication code for an actual authentication token. If everything goes well the `AuthenticationHandler` will be executed, otherwise listen for the `FailureHandler`.
+	
+	- parameter code:	The authorization code retrieved
+	*/
+	open func authenticateWithCode(_ code: String) {
+		let request = NSMutableURLRequest(url: provider.accessTokenURL as URL)
+		let parameters = provider.parameters.map { key, value in
+			"\(key)=\(value)"
+			}.joined(separator: "&")
+		let data = "client_id=\(provider.clientId)&client_secret=\(provider.clientSecret)&code=\(code)&\(parameters)"
+		
+		request.httpMethod = "POST"
+		request.httpBody = data.data(using: String.Encoding.utf8)
+		
+		URLSession.shared.dataTask(with: request as URLRequest) { [unowned self] (data, response, error) in
 			DispatchQueue.main.async {
-                
-                guard error == nil else {
-                    self.failureHandler?(.invalidRequest(error!))
-                    return
-                }
-                
-                guard let httpResponse = response as? HTTPURLResponse , 200..<300 ~= httpResponse.statusCode else {
-                    self.failureHandler?(.urlResponseError(response!))
-                    return
-                }
-                
-                guard let data = data else {
-                    self.failureHandler?(.invalidToken)
-                    return
-                }
-                
-                do {
-                    if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: AnyObject] {
-                            self.authenticationHandler?(json)
-                    }
-                } catch {
-                    self.failureHandler?(.urlResponseError(httpResponse))
-                }
-                
-            }
-        }.resume()
-    }
+				
+				guard error == nil else {
+					self.failureHandler?(.invalidRequest(error!))
+					return
+				}
+				
+				guard let httpResponse = response as? HTTPURLResponse , 200..<300 ~= httpResponse.statusCode else {
+					self.failureHandler?(.urlResponseError(response!))
+					return
+				}
+				
+				guard let data = data else {
+					self.failureHandler?(.invalidToken)
+					return
+				}
+				
+				do {
+					if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: AnyObject] {
+						self.authenticationHandler?(json)
+					}
+				} catch {
+					self.failureHandler?(.urlResponseError(httpResponse))
+				}
+				
+			}
+			}.resume()
+	}
 }
